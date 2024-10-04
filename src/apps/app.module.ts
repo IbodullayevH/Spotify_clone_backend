49797ff93d2a446f4f9cc5957e6dad425fbcf7a6
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-10-04 21:09:04 +0500

    typeorm connected

--- src/apps/app.module.ts
+++ src/apps/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '8077',
+      database: 'spotify_clone_db_v_1',
+      entities: [path.join(process.cwd(), "src", "entities", "**", "*.entity.ts")],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
